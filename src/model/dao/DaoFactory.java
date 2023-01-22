4498ecc09d4cbc1282d067425d6a4d905a9f5114
AcarisBen <acaris.ben@gmail.com>
2023-01-22 11:27:36 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,12 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 
-	public DaoFactory() {
-		// TODO Auto-generated constructor stub
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
 	}
-
+	
 }
